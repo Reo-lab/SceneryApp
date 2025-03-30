0b76bdb897ce5c718202680e9b064e806a91c7fd
rq-Reo-lab-at-321899771753 <yuniyuniyuniyuni333@gmail.com>
2025-03-30 10:39:17 +0900

    feature/modal作成

--- config/application.rb
+++ config/application.rb
@@ -15,7 +15,7 @@ module SceneryApp
     # not contain `.rb` files, or that should not be reloaded or eager loaded.
     # Common ones are `templates`, `generators`, or `middleware`, for example.
     config.autoload_lib(ignore: %w[assets tasks])
-
+    config.middleware.use Rack::CommonLogger, max_file_size: 10.megabytes
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
